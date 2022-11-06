25a8f79d2eb933fa6cf8ef05456f356e6ff886ae
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-06 14:18:25 +0100

    Add Input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(value) => this.props.onChange(value)}
+          placeholder={`Enter your ${this.props.placeholder}`}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    marginVertical: 15,
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#b69eff',
+    borderRadius: 15,
+    padding: 10,
+  },
+});
+
+export default Input;
