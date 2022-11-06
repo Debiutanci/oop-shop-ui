f1195c425ecd6b69d75bc5d5f7debad45bf50215
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-06 15:47:42 +0100

    Add axios post function

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -5,9 +5,8 @@ export function userLogin(userEmail, userPassword) {
     email: userEmail,
     password: userPassword,
   };
-  console.log(JSON.stringify(payload));
   axios
-    .post('https://oop-shop-core.herokuapp.com/auth/users/login/', JSON.stringify(payload))
+    .post('https://oop-shop-core.herokuapp.com/auth/users/login/', payload)
     .then((response) => console.log(response.data))
     .catch((error) => {
       console.error('There was an error!', error);
