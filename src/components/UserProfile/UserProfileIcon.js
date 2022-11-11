2bfc311e92f55bcf1924e6fd9c63c38781156f14
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-11 23:45:31 +0100

    Add UserProfileIcon component

---
+++ src/components/UserProfile/UserProfileIcon.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+import profileImage from '../../assets/icons/userProfileImage.png';
+
+class ProfileImage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.iconWrapper}>
+        <Image source={profileImage} style={styles.icon} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  iconWrapper: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    width: 100,
+    margin: 20,
+  },
+  icon: {
+    height: 100,
+    width: 100,
+    tintColor: '#7d53ff',
+  },
+});
+
+export default ProfileImage;
