a204690b3aea7f76700df3bf085b9fce93e45e16
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-15 10:42:37 +0100

    Add navigation goBack button

---
+++ src/components/GoBackBtn.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+import ArrowIcon from '../assets/icons/left-arrow.png';
+
+class GoBack extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.btnWrapper}>
+        <Image source={ArrowIcon} style={styles.icon} />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 25,
+    height: 25,
+    tintColor: '#7d52ff',
+  },
+  btnWrapper: {
+    margin: 20,
+    shadowColor: '#171717',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+});
+
+export default GoBack;
