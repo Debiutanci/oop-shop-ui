d9ce19d4efaf2798050c08cf42f7dc1760734beb
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-15 15:53:50 +0100

    Add component for displaying single category in details screen

---
+++ src/components/ItemDetails/SingleCategory.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import HorizontalLine from '../HorizontalLine/Line';
+
+class SingleCategory extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <View style={styles.categoryWrapper}>
+          <Text style={styles.categoryName}>{this.props.category}</Text>
+          <Text style={styles.text}>{this.props.children}</Text>
+        </View>
+        <HorizontalLine />
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  categoryWrapper: {
+    flexDirection: 'row',
+  },
+  categoryName: {
+    fontWeight: '600',
+    color: '#7b7b7b',
+  },
+  text: {
+    letterSpacing: 1,
+    color: '#767676',
+    marginLeft: 5,
+    flex: 1,
+  },
+});
+
+export default SingleCategory;
