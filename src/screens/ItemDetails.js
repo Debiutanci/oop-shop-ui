5cf4ff176b802f200f7238c9aae6d33bd39dd793
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-15 15:55:04 +0100

    Add product details screen content

--- src/screens/ItemDetails.js
+++ src/screens/ItemDetails.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import GoBackBtn from '../components/GoBackButton/GoBackBtn';
+import HorizontalLine from '../components/HorizontalLine/Line';
+import SingleCategory from '../components/ItemDetails/SingleCategory';
 
 class Products extends Component {
   constructor(props) {
@@ -9,10 +13,27 @@ class Products extends Component {
   render() {
     return (
       <View style={styles.background}>
+        <GoBackBtn navigation={this.props.navigation} />
         <View style={styles.screen}>
-          <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-            <Text>Go back</Text>
-          </TouchableOpacity>
+          <Text style={styles.productName}>{this.props.route.params.product.name}</Text>
+          <HorizontalLine />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text>{this.props.route.params.product.price} zł</Text>
+            <HorizontalLine />
+            <SingleCategory category="Opis:">
+              {this.props.route.params.product.description}
+            </SingleCategory>
+            <SingleCategory category="Kategoria:">
+              {this.props.route.params.product.category}
+            </SingleCategory>
+            <SingleCategory category="Producent:">
+              {this.props.route.params.product.manufacturer}
+            </SingleCategory>
+            <SingleCategory category="Kolor:">
+              {this.props.route.params.product.color}
+            </SingleCategory>
+            {/* <Text>{JSON.stringify(this.props.route.params.product)}</Text> */}
+          </ScrollView>
         </View>
       </View>
     );
@@ -22,7 +43,6 @@ class Products extends Component {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    paddingTop: 100,
     backgroundColor: '#f1edff',
   },
   screen: {
@@ -33,7 +53,17 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     padding: 20,
-    marginTop: 120,
+    marginTop: 200,
+    flex: 1,
+  },
+  productName: {
+    fontWeight: '700',
+    fontSize: 26,
+  },
+  text: {
+    letterSpacing: 1,
+    color: '#767676',
+    marginLeft: 5,
     flex: 1,
   },
 });
