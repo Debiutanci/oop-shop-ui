1c2aaa385b0d7dc74ed7c046ad7ef53b738072f1
Aleksandra Kozak <alex4ndra.2002@gmail.com>
2022-11-19 21:50:48 +0100

    Fix keyboard covering inputs on android

--- src/screens/LoginStart.js
+++ src/screens/LoginStart.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Image, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 import { userLogin } from '../../store/auth/actions';
 import { AuthContext } from '../../store/context/context';
@@ -56,21 +63,28 @@ class LoginStart extends Component {
             <StatusBar backgroundColor="#7d52ff" barStyle="light-content" />
             <Image source={ShopLogo} style={styles.image} />
             <View style={styles.screen}>
-              <Text style={styles.title}>Logowanie</Text>
-              <Input placeholder="email" onChange={this.setEmail} />
-              <Input
-                placeholder="password"
-                onChange={this.setPassword}
-                isSecured={this.state.isSecured}
-              >
-                <ShowAndHide setIsSecured={this.setIsSecured} isSecured={this.state.isSecured} />
-              </Input>
-              {this.state.isValidUser ? null : (
-                <Text style={styles.errorMessage}>Niepoprawne dane logowania</Text>
-              )}
-              {/* <Text>{this.state.email}</Text> */}
-              {/* <Text>{this.state.password}</Text> */}
-              <Btn text="Zaloguj" onPress={() => this.submitHandler(context)} />
+              <ScrollView>
+                <KeyboardAvoidingView behavior="position">
+                  <Text style={styles.title}>Logowanie</Text>
+                  <Input placeholder="email" onChange={this.setEmail} />
+                  <Input
+                    placeholder="password"
+                    onChange={this.setPassword}
+                    isSecured={this.state.isSecured}
+                  >
+                    <ShowAndHide
+                      setIsSecured={this.setIsSecured}
+                      isSecured={this.state.isSecured}
+                    />
+                  </Input>
+                  {this.state.isValidUser ? null : (
+                    <Text style={styles.errorMessage}>Niepoprawne dane logowania</Text>
+                  )}
+                  {/* <Text>{this.state.email}</Text> */}
+                  {/* <Text>{this.state.password}</Text> */}
+                  <Btn text="Zaloguj" onPress={() => this.submitHandler(context)} />
+                </KeyboardAvoidingView>
+              </ScrollView>
             </View>
           </View>
         )}
