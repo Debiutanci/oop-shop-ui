bba7e800c0dab8f2709a426e862ce5f4f6b31d43
Aleksandra Kozak <a.kozak@expansio.pl>
2023-01-22 15:55:29 +0100

    Fix ESlint issues

--- src/oop/classes.js
+++ src/oop/classes.js
@@ -1,61 +1,59 @@
 // import axios from 'axios';
 
-
 class AbstractOopModel {
-    constructor(identifier) {
-        this._identifier = identifier
-    }
+  constructor(identifier) {
+    this._identifier = identifier;
+  }
 
-    inside_log() {
-        console.log()
-    }
+  inside_log() {
+    console.log();
+  }
 }
-
 
 export class User {
-    constructor(email, password) {
-        this._email = email;
-        this._password = password;
-    }
+  constructor(email, password) {
+    this._email = email;
+    this._password = password;
+  }
 
-    get password() {
-        return this._password;
-    }
+  get password() {
+    return this._password;
+  }
 
-    set password(value) {
-        this._password = value;
-    }
+  set password(value) {
+    this._password = value;
+  }
 
-    get email() {
-        return this._email;
-    }
+  get email() {
+    return this._email;
+  }
 
-    set email(value) {
-        this._email = value;
-    }
+  set email(value) {
+    this._email = value;
+  }
 }
 
-
 export class SentryConnector {
-    constructor(endpoint, client) {
-        this._endpoint = endpoint
-        this._client = client
-    }
+  constructor(endpoint, client) {
+    this._endpoint = endpoint;
+    this._client = client;
+  }
 
-    api_error(error) {
-        console.log("Sentry has been notified ...");
-        const payload = {
-            error: error
-        }
-        // axios.post(this.endpoint, payload);
-    }
+  api_error(error) {
+    console.log('Sentry has been notified ...');
+    const payload = {
+      error,
+    };
+    // axios.post(this.endpoint, payload);
+  }
 }
 
 export class SentryClient {
-    constructor(service_key) {
-        this._secret = service_key
-    }
-    get secret() {
-        return this._secret
-    }
+  constructor(service_key) {
+    this._secret = service_key;
+  }
+
+  get secret() {
+    return this._secret;
+  }
 }
