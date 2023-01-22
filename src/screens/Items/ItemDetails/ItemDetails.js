0ce009b6cf3c015b1c4d3752fd920f2b8cae4f3e
Aleksandra Kozak <a.kozak@expansio.pl>
2023-01-22 23:07:21 +0100

    Fix ios app version

--- src/screens/Items/ItemDetails/ItemDetails.js
+++ src/screens/Items/ItemDetails/ItemDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image, ScrollView, Text, View } from 'react-native';
-import Toast from 'react-native-simple-toast';
+// import Toast from 'react-native-simple-toast';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import GoBackBtn from '../../../components/GoBackButton/GoBackBtn';
@@ -35,7 +35,7 @@ class Products extends Component {
 
   handleAddToCart = () => {
     addToCart(this.state.token, 1, this.props.route.params.product.identifier);
-    Toast.show('Dodano!', Toast.SHORT);
+    // Toast.show('Dodano!', Toast.SHORT);
   };
 
   componentDidMount() {
